aaf4b38cb421cd807c4f3eb4eb0a0daaf45d8ddf
Michał Hałas <michal.halas@sprint.pl>
2016-08-26 14:06:04 +0200

    New directive collapse

--- dist/js/nbUseful.js
+++ dist/js/nbUseful.js
@@ -825,6 +825,40 @@
 })(angular);
 /* global angular */
 
+(function(angular) {
+  angular.module('nbUseful').directive('collapse', [function() {
+    return {
+      restrict: 'A',
+      link: function($scope, element, attributes) {
+        var el = element[0],
+        currentHeight = function() {
+          return el.offsetHeight;
+        },
+        autoHeight = function() {
+          var height = currentHeight();
+
+          element.style.height = 'auto';
+          var autoHeight = currentHeight();
+
+          element.style.height = height + 'px';
+          currentHeight();
+
+          return autoHeight;
+        };
+
+        $scope.$watch(attributes.collapse, function(collapse) {
+          setTimeout(function() {
+            var height = collapse ? 0 : autoHeight();
+            element.style.height = height + 'px';
+            element.toggleClass('collapsed', collapse);
+          }, 0);
+        });
+      }
+    };
+  }]);
+})(angular);
+/* global angular */
+
 /**
  * @author Domas Trijonis <domas.trijonis@gmail.com> 
  * @link https://github.com/FDIM
---
+++ src/js/directive/collapse.js
@@ -0,0 +1,34 @@
+/* global angular */
+
+(function(angular) {
+  angular.module('nbUseful').directive('collapse', [function() {
+    return {
+      restrict: 'A',
+      link: function($scope, element, attributes) {
+        var el = element[0],
+        currentHeight = function() {
+          return el.offsetHeight;
+        },
+        autoHeight = function() {
+          var height = currentHeight();
+
+          element.style.height = 'auto';
+          var autoHeight = currentHeight();
+
+          element.style.height = height + 'px';
+          currentHeight();
+
+          return autoHeight;
+        };
+
+        $scope.$watch(attributes.collapse, function(collapse) {
+          setTimeout(function() {
+            var height = collapse ? 0 : autoHeight();
+            element.style.height = height + 'px';
+            element.toggleClass('collapsed', collapse);
+          }, 0);
+        });
+      }
+    };
+  }]);
+})(angular);
